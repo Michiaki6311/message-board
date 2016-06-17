e832cd23c67577d7fb0330185fea3a144c03ec9a
Michiaki <kanadebito63@yahoo.co.jp>
2016-06-17 07:58:51 +0000

    halt

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    #名前入力必須かつ20文字以内
+    validates :name, length: {maximum: 20}, presence: true
+    #内容入力必須かつ2文字以上30文字以下
+    validates :body, length: {minimum: 2,maximum: 30}, presence: true
 end
